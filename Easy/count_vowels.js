186b4e2e988da0ef3a1ce69e472f3e8fd4f6f54f
Satyam Tiwari <satyamtiwari1598@gmail.com>
2024-01-01 22:15:18 +0530

    count_vowels added

--- Easy/count_vowels.js
+++ Easy/count_vowels.js
@@ -1,1 +1,15 @@
 // Write a function to count the number of vowels in a given string.
+
+const count_vowels = (str) => {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let count = 0;
+  for (let i of str) {
+    for (let j of vowels) {
+      if (i == j) count += 1;
+    }
+  }
+  return count;
+};
+
+const str = "Satyam TiwarI";
+console.log(count_vowels(str));
