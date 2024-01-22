36ef94e487f6ea6299a9dde9bec8a0542cbcb49c
Satyam Tiwari <satyamtiwari1598@gmail.com>
2024-01-22 22:16:01 +0530

    Image Slider Added

--- Projects/reactJSInterviewProjects/src/App.jsx
+++ Projects/reactJSInterviewProjects/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -7,11 +8,13 @@ function App() {
   return (
     <>
       {/* {Accordian} */}
-      {/* <Accordian /> */}
+      <Accordian />
       {/* {RandomColor Component} */}
-      {/* <RandomColor /> */}
+      <RandomColor />
       {/* {Star Rating} */}
       <StarRating noOfStars={10} />
+      {/* Image Slider  */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
     </>
   );
 }
---
+++ Projects/reactJSInterviewProjects/src/components/image-slider/index.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+import "./styles.css";
+
+const ImageSlider = ({ url, limit = 5, page = 1 }) => {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchImages = async (getUrl) => {
+    try {
+      setLoading(true);
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+
+      if (data) {
+        setImages(data);
+        console.log(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setErrorMsg(e.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    url !== "" ? fetchImages(url) : null;
+  }, [url]);
+
+  if (loading) {
+    return <div>Loading data Please wait</div>;
+  }
+
+  if (errorMsg != null) {
+    return <div>Error Occured {errorMsg}</div>;
+  }
+
+  const handlePrev = () => {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  };
+
+  return (
+    <div className="container">
+      <BsArrowLeftCircleFill
+        onClick={handlePrev}
+        className="arrow arrow-left"
+      />
+      {images && images.length
+        ? images.map((image, index) => {
+            return (
+              <img
+                key={image.id}
+                src={image.download_url}
+                alt={image.download_url}
+                className={
+                  currentSlide === index
+                    ? "current-image"
+                    : "current-image hide-current-image"
+                }
+              />
+            );
+          })
+        : null}
+      <BsArrowRightCircleFill
+        onClick={handleNext}
+        className="arrow arrow-right"
+      />
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => {
+              return (
+                <button
+                  className={
+                    currentSlide === index
+                      ? "current-indicator"
+                      : "current-indicator inactive-indicator"
+                  }
+                  onClick={() => setCurrentSlide(index)}
+                  key={index}
+                ></button>
+              );
+            })
+          : null}
+      </span>
+    </div>
+  );
+};
+
+export default ImageSlider;
