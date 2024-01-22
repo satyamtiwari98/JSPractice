83310e16ff0d3bc7e3a652e8870150e05408a2e1
Satyam Tiwari <satyamtiwari1598@gmail.com>
2024-01-22 15:30:17 +0530

    Random Color Compoent Added

--- Projects/reactJSInterviewProjects/src/App.jsx
+++ Projects/reactJSInterviewProjects/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 
 function App() {
   return (
     <>
       {/* {Accordian} */}
-      <Accordian />
+      {/* <Accordian /> */}
+      {/* {RandomColor Component} */}
+      <RandomColor />
     </>
   );
 }
