2b914231791ae3418b85fce545464ff8f7990df3
Satyam Tiwari <satyamtiwari1598@gmail.com>
2024-02-09 23:24:58 +0530

    Mongoose Models Added

---
+++ mongoose/models/ecommerce/user.models.js
@@ -0,0 +1,25 @@
+import { Mongoose } from "mongoose";
+
+const userSchema = new Mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamps: true }
+);
+
+export const User = Mongoose.model("User", userSchema);
---
+++ mongoose/models/hospital-management/patient.models.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const patientSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    diagonsedWith: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    bloodGroup: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["MALE", "FEMALE", "OTHERS"],
+      default: "MALE",
+      required: true,
+    },
+    hospital: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hospital",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Patient = mongoose.model("Patient", patientSchema);
